d6f29c8dbf541ae0ebfb1575bf847f6d2a9aa48d
Noa Nissim <87809765+noanissim@users.noreply.github.com>
2021-12-09 22:36:32 +0200

    changes

--- api/board/board.service.js
+++ api/board/board.service.js
@@ -3,114 +3,112 @@ const ObjectId = require('mongodb').ObjectId
 const asyncLocalStorage = require('../../services/als.service')
 
 async function query(filterBy = {}) {
-    try {
-        const criteria = _buildCriteria(filterBy)
-        const collection = await dbService.getCollection('board')
-        const boards = await collection.find(criteria).toArray()
-        return boards
-    } catch (err) {
-        logger.error('cannot find boards', err)
-        throw err
-    }
-
+   try {
+      const criteria = _buildCriteria(filterBy)
+      const collection = await dbService.getCollection('board')
+      const boards = await collection.find(criteria).toArray()
+      return boards
+   } catch (err) {
+      logger.error('cannot find boards', err)
+      throw err
+   }
 }
 
 async function save(board) {
-    const { title, createdBy, style, labels, members, groups, activities } = board
-    let savedBoard
-    if (board._id) {
-        try {
-            savedBoard = {
-                _id: ObjectId(board._id),
-                title,
-                createdBy,
-                style,
-                labels,
-                members,
-                groups,
-                activities: activities.slice(0, 20),
-            }
-            const collection = await dbService.getCollection('board')
-            await collection.updateOne({ _id: savedBoard._id }, { $set: savedBoard })
-            return savedBoard
-        } catch(err) {
-            logger.error('cannot update board', err)
-            throw err
-        }
-    } else {
-        try {
-            savedBoard = {
-                createdAt: ObjectId(board._id).getTimestamp(),
-                title: board.title,
-                createdBy: board.createdBy,
-                style: {
-                    background: board.background
-                },
-                labels: [],
-                members: [board.createdBy],
-                groups: [],
-                activities: [],
-            }
-        } catch(err) {
-            logger.error('cannot add board', err)
-            throw err
-        }
-    }
+   const { title, createdBy, style, labels, members, groups, activities } = board
+   let savedBoard
+   if (board._id) {
+      try {
+         savedBoard = {
+            _id: ObjectId(board._id),
+            title,
+            createdBy,
+            style,
+            labels,
+            members,
+            groups,
+            activities: activities.slice(0, 20)
+         }
+         const collection = await dbService.getCollection('board')
+         await collection.updateOne({ _id: savedBoard._id }, { $set: savedBoard })
+         return savedBoard
+      } catch (err) {
+         logger.error('cannot update board', err)
+         throw err
+      }
+   } else {
+      try {
+         savedBoard = {
+            createdAt: ObjectId(board._id).getTimestamp(),
+            title: board.title,
+            createdBy: board.createdBy,
+            style: {
+               background: board.background
+            },
+            labels: [],
+            members: [board.createdBy],
+            groups: [],
+            activities: []
+         }
+      } catch (err) {
+         logger.error('cannot add board', err)
+         throw err
+      }
+   }
 }
 
 async function getById(boardId) {
-    try {
-        const collection = await dbService.getCollection('board')
-        const board = await collection.findOne({ '_id': ObjectId(boardId) })
-        return board
-    } catch(err) {
-        logger.error(`while finding board ${boardId}`, err)
-        throw err
-    }
+   try {
+      const collection = await dbService.getCollection('board')
+      const board = await collection.findOne({ _id: ObjectId(boardId) })
+      return board
+   } catch (err) {
+      logger.error(`while finding board ${boardId}`, err)
+      throw err
+   }
 }
 
 async function remove(boardId) {
-    try {
-        const collection = await dbService.getCollection('board')
-        
-        // remove only if user is owner/admin
-        // const criteria = { _id: ObjectId(reviewId) }
-        // if (!isAdmin) criteria.byUserId = ObjectId(userId)
-        await collection.deleteOne({ '_id': ObjectId(boardId) })
-    } catch (err) {
-        logger.error(`cannot remove board ${boardId}`, err)
-        throw err
-    }
+   try {
+      const collection = await dbService.getCollection('board')
+
+      // remove only if user is owner/admin
+      // const criteria = { _id: ObjectId(reviewId) }
+      // if (!isAdmin) criteria.byUserId = ObjectId(userId)
+      await collection.deleteOne({ _id: ObjectId(boardId) })
+   } catch (err) {
+      logger.error(`cannot remove board ${boardId}`, err)
+      throw err
+   }
 }
 
-
 async function add(review) {
-    try {
-        // peek only updatable fields!
-        const reviewToAdd = {
-            byUserId: ObjectId(review.byUserId),
-            aboutUserId: ObjectId(review.aboutUserId),
-            txt: review.txt
-        }
-        const collection = await dbService.getCollection('review')
-        await collection.insertOne(reviewToAdd)
-        return reviewToAdd;
-    } catch (err) {
-        logger.error('cannot insert review', err)
-        throw err
-    }
+   try {
+      // peek only updatable fields!
+      const reviewToAdd = {
+         byUserId: ObjectId(review.byUserId),
+         aboutUserId: ObjectId(review.aboutUserId),
+         txt: review.txt
+      }
+      const collection = await dbService.getCollection('review')
+      await collection.insertOne(reviewToAdd)
+      return reviewToAdd
+   } catch (err) {
+      logger.error('cannot insert review', err)
+      throw err
+   }
 }
 
 function _buildCriteria(filterBy) {
-    const criteria = {}
-    const { ctg } = filterBy
-    return criteria
+   const criteria = {}
+   // const { ctg } = filterBy
+   return criteria
 }
 
 module.exports = {
-    query,
-    remove,
-    add,
-    getById,
-    save
+   query,
+   remove,
+   add,
+   getById,
+   save
 }
